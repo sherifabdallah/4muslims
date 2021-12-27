3ce339f130adf7fff0598771f624c43040dc63e3
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2021-12-27 23:37:03 +0200

    Add files via upload

---
+++ 4muslims/islam/alanfitar/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link src="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">  
+    <link rel="stylesheet" href="style.css">    
+    <link rel="icon" href="https://i.ibb.co/xhKTfVb/1631179818168.png">
+<title>4Muslims</title>
+</head>
+<body id="body"> 
+<div id="myProgress">
+<div id="myBar"></div></div>
+<nav id="mynav" class="navbar navbar-inverse" style="background-color: #0e4130!important;">
+    <div class="container-fluid">
+    <div class="navbar-header">
+    <div style="padding-right: 300px;" class="navbar-brand" onclick="this.onclick=null; move() , window.location.href='/islam/quran'">
+    <span class="glyphicon">&#xe091;</span>
+    </div></div></div></nav><br>
+
+
+
+
+
+<br><div class="col-md-4" style="height: fit-content;" > <div  class="card text-black"  style="background-color: white; color: black;  border-radius: 25px;" ><div  class="card-body" ><br><div style="font-size: 30px;  ">
+بِسْمِ اللهِ الرَّحْمنِ الرَّحِيم</div>
+<br>
+<p class="card-text" style="text-align: center; font-size: 29px;" >
+إِذَا ٱلسَّمَآءُ ٱنفَطَرَتْ ﴿١﴾ وَإِذَا ٱلْكَوَاكِبُ ٱنتَثَرَتْ ﴿٢﴾ وَإِذَا ٱلْبِحَارُ فُجِّرَتْ ﴿٣﴾ وَإِذَا ٱلْقُبُورُ بُعْثِرَتْ ﴿٤﴾ عَلِمَتْ نَفْسٌ مَّا قَدَّمَتْ وَأَخَّرَتْ ﴿٥﴾ يَٰٓأَيُّهَا ٱلْإِنسَٰنُ مَا غَرَّكَ بِرَبِّكَ ٱلْكَرِيمِ ﴿٦﴾ ٱلَّذِى خَلَقَكَ فَسَوَّىٰكَ فَعَدَلَكَ ﴿٧﴾ فِىٓ أَىِّ صُورَةٍۢ مَّا شَآءَ رَكَّبَكَ ﴿٨﴾ كَلَّا بَلْ تُكَذِّبُونَ بِٱلدِّينِ ﴿٩﴾ وَإِنَّ عَلَيْكُمْ لَحَٰفِظِينَ ﴿١٠﴾ كِرَامًا كَٰتِبِينَ ﴿١١﴾ يَعْلَمُونَ مَا تَفْعَلُونَ ﴿١٢﴾ إِنَّ ٱلْأَبْرَارَ لَفِى نَعِيمٍۢ ﴿١٣﴾ وَإِنَّ ٱلْفُجَّارَ لَفِى جَحِيمٍۢ ﴿١٤﴾ يَصْلَوْنَهَا يَوْمَ ٱلدِّينِ ﴿١٥﴾ وَمَا هُمْ عَنْهَا بِغَآئِبِينَ ﴿١٦﴾ وَمَآ أَدْرَىٰكَ مَا يَوْمُ ٱلدِّينِ ﴿١٧﴾ ثُمَّ مَآ أَدْرَىٰكَ مَا يَوْمُ ٱلدِّينِ ﴿١٨﴾ يَوْمَ لَا تَمْلِكُ نَفْسٌ لِّنَفْسٍۢ شَيْـًٔا ۖ وَٱلْأَمْرُ يَوْمَئِذٍۢ لِّلَّهِ
+</p>
+
+</div>
+</div>
+</div>
+
+
+
+    
+<br><br><br><br>
+
+<header class="header">
+<div class="container" >
+<nav class="bottom-nav" style="background-color: #0e4130!important;">
+<div onclick="this.onclick=null; move() , window.location.href='/islam/home/'" class="bottom-nav-item">
+<div class="bottom-nav-link">
+<i class="material-icons">&#xe871;</i>
+<span>Dashboard</span>
+</div>
+</div>
+<div onclick="this.onclick=null; move() , window.location.href='/islam/account/'" class="bottom-nav-item">
+<div class="bottom-nav-link">
+<i class="material-icons">&#xe853;</i>
+<span>Account</span>
+</div>
+</div>
+
+<div onclick="this.onclick=null; move() , window.location.href='/islam/settings'" class="bottom-nav-item">
+<div class="bottom-nav-link">
+<i class="material-icons">&#xe8b8;</i>
+<span>Setting</span>
+</div>
+</div>
+</nav>
+</div>
+</header>
+
+<script src="script.js"></script>
+
+</body>
+</html>
---
+++ 4muslims/islam/albalad/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link src="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">  
+    <link rel="stylesheet" href="style.css">    
+    <link rel="icon" href="https://i.ibb.co/xhKTfVb/1631179818168.png">
+<title>4Muslims</title>
+</head>
+<body id="body"> 
+<div id="myProgress">
+<div id="myBar"></div></div>
+<nav id="mynav" class="navbar navbar-inverse" style="background-color: #0e4130!important;">
+    <div class="container-fluid">
+    <div class="navbar-header">
+    <div style="padding-right: 300px;" class="navbar-brand" onclick="this.onclick=null; move() , window.location.href='/islam/quran'">
+    <span class="glyphicon">&#xe091;</span>
+    </div></div></div></nav><br>
+
+
+
+
+
+<br><div class="col-md-4" style="height: fit-content;" > <div  class="card text-black"  style="background-color: white; color: black;  border-radius: 25px;" ><div  class="card-body" ><br><div style="font-size: 30px;  ">
+بِسْمِ اللهِ الرَّحْمنِ الرَّحِيم</div>
+<br>
+<p class="card-text" style="text-align: center; font-size: 29px;" >
+لَآ أُقْسِمُ بِهَٰذَا ٱلْبَلَدِ ﴿١﴾ وَأَنتَ حِلٌّۢ بِهَٰذَا ٱلْبَلَدِ ﴿٢﴾ وَوَالِدٍۢ وَمَا وَلَدَ ﴿٣﴾ لَقَدْ خَلَقْنَا ٱلْإِنسَٰنَ فِى كَبَدٍ ﴿٤﴾ أَيَحْسَبُ أَن لَّن يَقْدِرَ عَلَيْهِ أَحَدٌ ﴿٥﴾ يَقُولُ أَهْلَكْتُ مَالًا لُّبَدًا ﴿٦﴾ أَيَحْسَبُ أَن لَّمْ يَرَهُۥٓ أَحَدٌ ﴿٧﴾ أَلَمْ نَجْعَل لَّهُۥ عَيْنَيْنِ ﴿٨﴾ وَلِسَانًا وَشَفَتَيْنِ ﴿٩﴾ وَهَدَيْنَٰهُ ٱلنَّجْدَيْنِ ﴿١٠﴾ فَلَا ٱقْتَحَمَ ٱلْعَقَبَةَ ﴿١١﴾ وَمَآ أَدْرَىٰكَ مَا ٱلْعَقَبَةُ ﴿١٢﴾ فَكُّ رَقَبَةٍ ﴿١٣﴾ أَوْ إِطْعَٰمٌ فِى يَوْمٍۢ ذِى مَسْغَبَةٍۢ ﴿١٤﴾ يَتِيمًا ذَا مَقْرَبَةٍ ﴿١٥﴾ أَوْ مِسْكِينًا ذَا مَتْرَبَةٍۢ ﴿١٦﴾ ثُمَّ كَانَ مِنَ ٱلَّذِينَ ءَامَنُوا وَتَوَاصَوْا بِٱلصَّبْرِ وَتَوَاصَوْا بِٱلْمَرْحَمَةِ ﴿١٧﴾ أُولَٰٓئِكَ أَصْحَٰبُ ٱلْمَيْمَنَةِ ﴿١٨﴾ وَٱلَّذِينَ كَفَرُوا بِـَٔايَٰتِنَا هُمْ أَصْحَٰبُ ٱلْمَشْـَٔمَةِ ﴿١٩﴾ عَلَيْهِمْ نَارٌ مُّؤْصَدَةٌۢ
+</p>
+
+</div>
+</div>
+</div>
+
+
+
+    
+<br><br><br><br>
+
+<header class="header">
+<div class="container" >
+<nav class="bottom-nav" style="background-color: #0e4130!important;">
+<div onclick="this.onclick=null; move() , window.location.href='/islam/home/'" class="bottom-nav-item">
+<div class="bottom-nav-link">
+<i class="material-icons">&#xe871;</i>
+<span>Dashboard</span>
+</div>
+</div>
+<div onclick="this.onclick=null; move() , window.location.href='/islam/account/'" class="bottom-nav-item">
+<div class="bottom-nav-link">
+<i class="material-icons">&#xe853;</i>
+<span>Account</span>
+</div>
+</div>
+
+<div onclick="this.onclick=null; move() , window.location.href='/islam/settings'" class="bottom-nav-item">
+<div class="bottom-nav-link">
+<i class="material-icons">&#xe8b8;</i>
+<span>Setting</span>
+</div>
+</div>
+</nav>
+</div>
+</header>
+
+<script src="script.js"></script>
+
+</body>
+</html>
